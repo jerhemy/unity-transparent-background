1cec734e23d9ec7083004d531672bf7b0d6fa87f
Goblin <goblin@thegoblinworks.com>
2022-04-10 14:45:30 -0600

    Fork

---
+++ Assets/Pixelation/Scripts/Pixelation.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Pixelation.Example.Scripts;
+using UnityEngine;
+
+namespace Assets.Pixelation.Scripts
+{
+    [ExecuteInEditMode]
+    [AddComponentMenu("Image Effects/Color Adjustments/Pixelation")]
+    public class Pixelation : ImageEffectBase
+    {
+        [Range(64.0f, 2048.0f)] public float BlockCount = 128;
+        private float blockCountModifier;
+
+        private void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            float k = Camera.main.aspect;
+            Vector2 count = new Vector2(BlockCount*k, BlockCount/k);
+            Vector2 size = new Vector2(1.0f/count.x, 1.0f/count.y);
+            //
+            material.SetVector("BlockCount", count);
+            material.SetVector("BlockSize", size);
+            Graphics.Blit(source, destination, material);
+        }
+    }
+}
